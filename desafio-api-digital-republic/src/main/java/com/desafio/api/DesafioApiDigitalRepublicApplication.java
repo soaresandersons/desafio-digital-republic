d766831e7c3097ee0f0e63254ff406f10a1c2c33
Anderson Soares <anderson.soares@live.com>
2021-12-21 14:42:27 -0400

    Add files via upload

---
+++ desafio-api-digital-republic/src/main/java/com/desafio/api/DesafioApiDigitalRepublicApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioApiDigitalRepublicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioApiDigitalRepublicApplication.class, args);
+	}
+
+}
